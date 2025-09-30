47608c70f94742be58eb2fb45991bf6a785318e7
XxArgelDarkxX <castaneda.angel@correounivalle.edu.co>
2025-09-29 19:47:21 -0500

    Initial Class File

---
+++ app/src/main/scala/taller/ConjuntosDifusos.scala
@@ -0,0 +1,31 @@
+class ConjuntosDifusos {
+  type ConjDifuso = Int => Double
+    def pertenece(elem: Int, s: ConjDifuso): Double = {
+    s(elem)
+    }
+
+    def grande(d: Int, e: Int): ConjDifuso = {
+
+    }
+
+    def complemento(c: ConjDifuso): ConjDifuso = {
+
+    }
+
+    def union(cd1: ConjDifuso, cd2: ConjDifuso): ConjDifuso = {
+
+    }
+
+    def interseccion(cd1: ConjDifuso, cd2: ConjDifuso): ConjDifuso = {
+
+    }
+    
+    def inclusion(cd1: ConjDifuso, cd2: ConjDifuso): Boolean = {
+
+    }
+
+    def igualdad(cd1: ConjDifuso, cd2: ConjDifuso): Boolean = {
+        
+    }
+}
+
